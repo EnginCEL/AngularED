e676e35313481b62a36990892cd831585d5ceed3
EnginCEL <80245505+EnginCEL@users.noreply.github.com>
2021-07-13 15:46:06 +0300

    13-07-21

--- src/app/Components/product/product.component.ts
+++ src/app/Components/product/product.component.ts
@@ -15,6 +15,7 @@ export class ProductComponent implements OnInit {
   ) { }
   products: Product[] = [];
   dataLoaded = false;
+  filterText="";
   ngOnInit(): void {
     this.activetedRoute.params.subscribe((params) => {
       if (params['CategoryID']) {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './Components/product/product.component';
 import { CategoryComponent } from './Components/category/category.component';
 import { NaviComponent } from './Components/navi/navi.component';
 import { VatAddedPipe } from './Pipes/vat-added.pipe';
+import { FilterPipePipe } from './Pipes/filter-pipe.pipe';
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
     CategoryComponent,
     NaviComponent,
-    VatAddedPipe
+    VatAddedPipe,
+    FilterPipePipe
   ],
   imports: [
     HttpClientModule,
     BrowserModule,
-    AppRoutingModule,
+    AppRoutingModule,FormsModule
     
   ],
   providers: [],
